ada5e47faf1092545cb12ac2d47816e572e11c8f
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2016-01-05 23:25:58 +0100

    routes for salons completed

--- app/models/salon.rb
+++ app/models/salon.rb
@@ -1,4 +1,7 @@
 class Salon < ActiveRecord::Base
   has_many :prices
   has_many :services, through: :prices
+  def to_param
+    permalink
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
   resources :prices
   resources :prices
   resources :services
-  resources :salons
+
+  get "/saloni" => "salons#index", as: "salons"
+  post "/saloni" => "salons#create"
+  get "/salon/new" => "salons#new", as: "new_salon"
+  get "/salon/edit/:id" => "salons#edit", as: "edit_salon"
+  get "/salon/:id" => "salons#show", as: "salon"
+  patch "/salon/:id" => "salons#update"
+  put "/salon/:id" => "salons#update"
+  delete "/salon/:id" => "salons#destroy"
+
 end
