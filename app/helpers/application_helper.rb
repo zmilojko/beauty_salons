ba736426d21e5885cfa0248f368c750ad3d0864c
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2016-02-17 18:59:35 +0100

    helper added to help control users location
    Location can now be reseted, and distance will not be displayed if users location is unknown.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def location_is_know
+    # GET COOKEIS
+    lat_cookie = cookies[:lat]
+    lng_cookie = cookies[:lng]
+    # TEST IF COOKIES ARE SET
+    if (!cookies[:lat] or !cookies[:lng])
+      return false
+    else
+      return true
+    end
+  end
+
 end
