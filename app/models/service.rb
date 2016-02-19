ddf34494725b98c2a984996450359d6329689b51
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2016-02-19 14:31:12 +0100

    getLocation is now displaying status

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,10 +2,6 @@ class Service < ActiveRecord::Base
   has_many :prices
   has_many :salons, through: :prices
 
-  def distance_to(target)
-    calculate_distance(self.lng, self.lat, target[:lng], target[:lat])
-  end
-
   def to_param
     slug
   end
