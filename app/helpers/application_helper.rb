8733efb57133a7529021b7760dab857fb425dad3
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2016-02-24 23:09:16 +0100

    if is user admin is now moved to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
 
+  def is_admin(user)
+    if user_signed_in?
+      if user.control.to_i == 0
+        return true
+      else
+        return false
+      end
+    else
+      return false
+    end
+  end
+
   def location_is_know
     # GET COOKEIS
     lat_cookie = cookies[:lat]
