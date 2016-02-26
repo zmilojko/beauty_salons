074f081c994e2c509a8b5c1fff7a999817b39b03
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2016-02-26 17:17:04 +0100

    mailchimp subscribe messages improved

--- app/controllers/mailchimpsubscribe_controller.rb
+++ app/controllers/mailchimpsubscribe_controller.rb
@@ -9,33 +9,33 @@ class MailchimpsubscribeController < ApplicationController
     begin
     @mailchimp.lists.subscribe(@list_id, {:email => email, :double_optin => false})
     respond_to do |format|
-    format.json{render :json => {:message => "Success! Check your email to confirm sign up."}}
+    format.json{render :json => {:message => '<div class="alert alert-success alert-dismissible"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Čestitamo!</strong><br> Vaša adresa je uspešno dodata na mailing listu. Molimo proverite svoj email zbog potvrde.</div>'}}
     end
 
     rescue Mailchimp::ListAlreadySubscribedError
     respond_to do |format|
-    format.json{render :json => {:message => "#{email} is already subscribed to the list"}}
+    format.json{render :json => {:message => '<div class="alert alert-danger alert-dismissible"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+email+' je već na našoj listi.</div>'}}
     end
 
     rescue Mailchimp::ListDoesNotExistError
     respond_to do |format|
-    format.json{render :json => {:message => "The list could not be found."}}
+    format.json{render :json => {:message => '<div class="alert alert-danger alert-dismissible"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Lista nije pronađena.</div>'}}
     end
 
     rescue Mailchimp::Error => ex
     if ex.message
     respond_to do |format|
-    format.json{render :json => {:message => "There is an error. Please enter valid email id."}}
+    format.json{render :json => {:message => '<div class="alert alert-danger alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Došlo je do greške. Molimo unesite ispravnu email adresu.</div>'}}
     end
     else
     respond_to do |format|
-    format.json{render :json => {:message => "An unknown error occurred."}}
+    format.json{render :json => {:message => '<div class="alert alert-danger alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> Došlo je do greške. Molimo pokušajte kasnije.</div>'}}
     end
     end
     end
     else
     respond_to do |format|
-    format.json{render :json => {:message => "Email Address Cannot be blank. Please enter valid email id."}}
+    format.json{render :json => {:message => '<div class="alert alert-danger alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> Email adresa ne može biti prazna. Molimo unesite ispravnu email adresu.</div>'}}
     end
     end
   end
